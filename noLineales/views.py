8a0e46f2276ce5a8ec66fc638ef1f88989d81e1d
bgomezv123 <bgomezv@unsa.edu.pe>
2021-11-10 09:47:33 -0500

    metodo secante y newton implementado

--- noLineales/views.py
+++ noLineales/views.py
@@ -214,3 +214,156 @@ def home_view(request):
         
     return render(request, 'home_view.html', context=context)
 
+
+def metodo_newton_raphson(express,puntoA):
+    print("Metodo Newton Raphson")
+    #declaracion de variables
+    x = symbols('x')
+    #variables necesarias
+    str_expr = ""
+    count = 0
+    itr = 0
+    
+    xn = []
+    xr = []
+    fxn = []
+    dfxn = []
+
+    err = []
+    error = 0
+    #parse funcion ingresada
+    str_expr = express
+    expr= sympify(str_expr, evaluate=False)
+    f = lambdify(x,expr)
+    df = lambdify(x,diff(expr,x))
+    #asignacion de puntos iniciales
+    xn.append(float(puntoA))
+
+
+    #iteraciones
+    while(count<100):
+        fxn.append(f(xn[count]))
+        dfxn.append(df(xn[count]))
+
+
+        xr.append(xn[count]-((fxn[count])/(dfxn[count])))
+
+        xn.append(xr[count])
+
+        
+        if(count>0):
+            error = abs((xr[count]-xr[count-1])/xr[count])
+            err.append(error)
+            if(error == 0):
+                itr = count + 1
+                count = 100
+        else:
+            err.append(100)
+
+        count=count+1
+
+    #pasamos nuestros valores
+    context = {
+        "metodo":"newtonRaphson",
+        "count":range(itr),
+        "xn":xn,
+        "xr":xr,
+        "fxn":fxn,
+        "dfxn":dfxn,
+        "err":err,
+    }
+
+    return context
+    
+
+def metodo_secante(express,puntoA):
+    print("Metodo Secante")
+    #declaracion de variables
+    x = symbols('x')
+    str_expr = ""
+    count = 0
+    itr = 0
+
+    xi = []
+    xi_1 = []
+    fxi = []
+    fxi_1 = []
+    xr = []
+
+    err = []
+    error = 0
+
+    #parse funcion ingresada
+    str_expr = express
+    expr= sympify(str_expr, evaluate=False)
+    f = lambdify(x,expr)
+    #asignacion de puntos iniciales
+
+    puntoStr = str(float(puntoA))
+    puntoStr = puntoStr[puntoStr.find(".")+1:]
+
+    print((10**(len(puntoStr)))**-1)
+
+    minDif = ((10**(len(puntoStr)))**-1)
+    xi.append(float(puntoA))
+    xi_1.append(float(puntoA)+float(minDif))
+
+    print(xi[0], "  " , xi_1[0])
+    
+    while(count<100):
+        fxi.append(f(xi[count]))
+        fxi_1.append(f(xi_1[count]))
+
+
+        xr.append(xi[count]-(fxi[count]*(xi_1[count]-xi[count]))/(fxi_1[count]-fxi[count]))
+
+        xi.append(xr[count])
+
+        xi_1.append(xi[count])
+        
+        if(count>0):
+            error = abs((xr[count]-xr[count-1])/xr[count])
+            err.append(error)
+            if(error == 0):
+                itr = count + 1
+                count = 100
+        else:
+            err.append(100)
+
+        count=count+1
+
+
+    context = {
+        "metodo":"secante",
+        "count":range(itr),
+        "xi":xi,
+        "xi_1":xi_1,
+        "fxi":fxi,
+        "fxi_1":fxi_1,
+        "xr":xr,
+        "err":err,
+    }
+    
+    return context
+
+
+def mabiertos(request):
+    context ={}
+
+    if request.POST:
+
+        #request
+        metodo = request.POST["method"]
+        funcion = request.POST["inputField"]
+        puntoA =  request.POST["puntoA"]
+
+        if metodo == "puntoFijo":
+            context = metodo_punto_fijo(funcion, puntoA)
+        if metodo == "newtonRaphson":
+            context =  metodo_newton_raphson(funcion, puntoA)
+        if metodo == "secante":
+            context = metodo_secante(funcion, puntoA)
+
+        
+    return render(request, 'mabiertos.html', context=context)
+
