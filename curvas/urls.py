ada51dcd7e9b995f232c66f631dd5c3d77e7ed07
Sofia2506 <sonque@unsa.edu.pe>
2021-12-15 12:36:26 -0500

    sidebars para cada app listos

--- curvas/urls.py
+++ curvas/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('Minimos_Cuadrados', minimos_cuadrados_view, name="minimos_cuadrados"),
     path('Minimos_Cuadrados_Ajax', minimos_cuadrados_ajax, name="minimos_cuadrados_ajax"),
     path('Diferencias_Divididas', diferencias_divididas_view, name="diferencias_divididas"),
-     path('Diferencias_Divididas_Ajax', diferencias_divididas_ajax, name="diferencias_divididas_ajax"),
+    path('Diferencias_Divididas_Ajax', diferencias_divididas_ajax, name="diferencias_divididas_ajax"),
 ]
