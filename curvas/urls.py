91a47e77e1bf52f6d150fa24664755cd61804c59
bgomezv123 <bgomezv@unsa.edu.pe>
2021-12-14 23:18:44 -0500

    Se creo el metodo de diferencias divididas

--- curvas/urls.py
+++ curvas/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from curvas.views import (
     minimos_cuadrados_ajax,
     minimos_cuadrados_view,
+    diferencias_divididas_view,
+    diferencias_divididas_ajax
 )
 
 app_name = 'curvas'
@@ -11,4 +13,6 @@ urlpatterns = [
     #path('', , name ="" ),
     path('Minimos_Cuadrados', minimos_cuadrados_view, name="minimos_cuadrados"),
     path('Minimos_Cuadrados_Ajax', minimos_cuadrados_ajax, name="minimos_cuadrados_ajax"),
+    path('Diferencias_Divididas', diferencias_divididas_view, name="diferencias_divididas"),
+     path('Diferencias_Divididas_Ajax', diferencias_divididas_ajax, name="diferencias_divididas_ajax"),
 ]
